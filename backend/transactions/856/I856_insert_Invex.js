b0d0902fcdfbc515ee9355aca9eafecf308536bb
STTXjdewitt <jdewitt@sttxna.com>
2025-07-14 09:57:09 -0400

    fixed null value

--- backend/transactions/856/I856_insert_Invex.js
+++ backend/transactions/856/I856_insert_Invex.js
@@ -164,7 +164,7 @@ async function insert856InvexInbound(pool, header, details, measurements, names)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19);`, [
                 header.hdr_type,
                 header.hdr_key,
-                details.dtl_pol,
+                details.dtl_pol ? details.dtl_pol : details.dtl_hl1, 
                 null,
                 details.dtl_po,
                 details.dtl_cpo,

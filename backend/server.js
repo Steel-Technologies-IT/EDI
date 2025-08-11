f6e60c95a92f56283099ecd3d4a8c1bdc3c8fe27
STTXjdewitt <jdewitt@sttxna.com>
2025-08-11 13:25:09 -0400

    Revert "863integration"

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const { LoadI856SNF } = require('./transactions/856/I856_insert_SNF.js');
 
 
 //863 functions
-const { getInvexRecords863 } = require('./transactions/863/I863_json_crt.js');
+const { transformToStructuredJSON863 } = require('./transactions/863/I863_json_crt.js');
 const { transformI863 } = require('./transactions/863/I863_transform.js');
 const { LoadI863SNF } = require('./transactions/863/I863_insert_SNF.js');
 
@@ -87,7 +87,7 @@ const pool2 = require("./db2.js");   //Postgres DB for decoder table
 // Mapping of transaction types to their JSON building function 
 const transformMap = {
   '856': getInvexRecords856,
-  '863': getInvexRecords863,
+  '863': transformToStructuredJSON863,
   '861': transformToStructuredJSON861,
   '870': transformToStructuredJSON870,
   '846': transformToStructuredJSON846,
@@ -269,7 +269,7 @@ async function uploadFile(filePath, delayMs = 500) {
 
       // MARK: 7. Send Structured JSON to CleoHarmony Directory for Invex upload
       // Or call your writeStructuredJSON function:
-       writeStructuredJSON(structured, path.basename(filePath));
+      writeStructuredJSON(structured, path.basename(filePath));
 
 
       // MARK: 8. Clean up

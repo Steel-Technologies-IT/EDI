851f33149f90a027d711cdad1d21d4f240f65c2b
STTXjdewitt <jdewitt@sttxna.com>
2025-09-04 08:31:58 -0400

    Update writeJSON.js

--- backend/writeJSON.js
+++ backend/writeJSON.js
@@ -9,17 +9,6 @@ async function writeStructuredJSON(structured, originalName, outputDir, ext = '.
   const tempPath = path.join(__dirname, `../../../../../JSONS/${baseName}${ext}`);
 
   await fs.writeFile(tempPath, JSON.stringify(structured, null, 2));
-
-  // Use Start-Process with -Verb RunAs for elevation
-  const psCmd = `powershell -Command "Start-Process powershell -ArgumentList 'Copy-Item -Path \\"${tempPath}\\" -Destination \\"${filePath}\\"' -Verb RunAs"`;
-  exec(psCmd, (err, stdout, stderr) => {
-    if (err) {
-      console.error('PowerShell copy error:', err);
-    } else {
-      console.log('Structured JSON written to:', filePath);
-    }
-    //fs.unlink(tempPath);
-  });
 }
 
 module.exports = { writeStructuredJSON };

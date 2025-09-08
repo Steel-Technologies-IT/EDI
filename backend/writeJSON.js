3d5ae2be069dd2c44b51fc088568355b5cc1a971
Jordan Richardson <jrichardson@sttxna.com>
2025-09-08 12:21:08 -0400

    Hardcode temp path temporarily.

--- backend/writeJSON.js
+++ backend/writeJSON.js
@@ -6,7 +6,7 @@ async function writeStructuredJSON(structured, originalName, outputDir, ext = '.
   outputDir = process.env.REACT_APP_CLEO_PATH;
   const baseName = path.parse(originalName).name;
   const filePath = `${outputDir}\\${baseName}${ext}`;
-  const tempPath = path.join(__dirname, `../../../../../${baseName}${ext}`);
+  const tempPath = `E:/JSONS/${baseName}${ext}`;
 
   await fs.writeFile(tempPath, JSON.stringify(structured, null, 2));
 

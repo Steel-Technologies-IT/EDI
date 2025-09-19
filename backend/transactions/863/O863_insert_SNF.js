d93059cd504be428ecb8820b7b6af0ed23d42bb0
GNSRohan <rohano@globalnestsolutions.com>
2025-09-20 00:05:41 +0530

    Added tag_lot_id
    Added tag_lot_id

--- backend/transactions/863/O863_insert_SNF.js
+++ backend/transactions/863/O863_insert_SNF.js
@@ -39,16 +39,15 @@ async function InsertIntoSNFTables(pool, InterchangeControl, TransactionSet, Shi
   
   await Promise.all(ProductItem.map(async (ProductItem,index) => {
   await Promise.all(PhysicalTests.filter(PhysicalTests => PhysicalTests["phts_linenumber"] === ProductItem["prd_itemnumber"]).map(async PhysicalTests => {
-    await insert863Measure(pool, InterchangeControl.ictl_edixcontrolnumber, ProductItem.prd_itemnumber, ProductItem.prd_heat, ProductItem.prd_externaltagid, ProductItem.prd_vendortagid, '71', PhysicalTests.phts_x12physicaltest, PhysicalTests.phts_value, null, PhysicalTests.phts_x12unitofmeasure, null, null, null, PhysicalTests.phts_x12testdirection, null, null, null, null, null, flag)}));
+    await insert863Measure(pool, InterchangeControl.ictl_edixcontrolnumber, ProductItem.prd_itemnumber, ProductItem.prd_heat, ProductItem.prd_externaltagid, ProductItem.prd_vendortagid, '71', PhysicalTests.phts_x12physicaltest, PhysicalTests.phts_value, null, PhysicalTests.phts_x12unitofmeasure, null, null, null, PhysicalTests.phts_x12testdirection, null, null, null, null, null, flag, ProductItem.prd_taglotid)}));
   }));
 
   await Promise.all(ProductItem.map(async (ProductItem,index) => {
   await Promise.all(Chemistry.filter(Chemistry => Chemistry["chm_linenumber"] === ProductItem["prd_itemnumber"]).map(async Chemistry => {
-    await insert863Measure(pool, InterchangeControl.ictl_edixcontrolnumber, ProductItem.prd_itemnumber, ProductItem.prd_heat, ProductItem.prd_externaltagid, ProductItem.prd_vendortagid, '68', Chemistry.chm_x12chemelement, Chemistry.chm_value, null, null, null, null, null, null, null, null, null, null, null, flag)}));
-  }));
-  
+    await insert863Measure(pool, InterchangeControl.ictl_edixcontrolnumber, ProductItem.prd_itemnumber, ProductItem.prd_heat, ProductItem.prd_externaltagid, ProductItem.prd_vendortagid, '68', Chemistry.chm_x12chemelement, Chemistry.chm_value, null, null, null, null, null, null, null, null, null, null, null, flag, ProductItem.prd_taglotid)}));
+  }));  
 
-  }  
+}  
 // //MARK: Header
 // //863 Header Insert
 async function insert863Header(pool, InterchangeControl, ShipmentHeaderTestResult, flag, filePath) 
@@ -152,8 +151,8 @@ async function insert863Detail(pool, index, InterchangeControl, ShipmentHeaderTe
  try {
   
   await pool.query(`INSERT INTO public."863_SNF_Detail"(
-  dtl_type,dtl_key,dtl_line,dtl_heat,dtl_mcoil,dtl_mo,dtl_mol,dtl_po,dtl_pol,dtl_pod,dtl_part,dtl_tst_unt,dtl_tdat,dtl_pdat,dtl_n1st,dtl_n1mf,dtl_locn,dtl_crt_dat,dtl_crt_tim,dtl_crt_pgm,dtl_flow_flag,dtl_prd_dte,dtl_shp_dte,dtl_heat_trt_csh_dte,dtl_lub_app_dte,dtl_prev_proc_tag_id)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26)`,
+  dtl_type,dtl_key,dtl_line,dtl_heat,dtl_mcoil,dtl_mo,dtl_mol,dtl_po,dtl_pol,dtl_pod,dtl_part,dtl_tst_unt,dtl_tdat,dtl_pdat,dtl_n1st,dtl_n1mf,dtl_locn,dtl_crt_dat,dtl_crt_tim,dtl_crt_pgm,dtl_flow_flag,dtl_prd_dte,dtl_shp_dte,dtl_heat_trt_csh_dte,dtl_lub_app_dte,dtl_prev_proc_tag_id,dtl_tag_lot)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27)`,
 [
   
       "O", //$1
@@ -181,7 +180,8 @@ async function insert863Detail(pool, index, InterchangeControl, ShipmentHeaderTe
       ShipmentHeaderTestResult.tres_shippingdatetime ? ShipmentHeaderTestResult.tres_shippingdatetime.slice(0, 8) : null, //$23 Shipping Date
       null, //$24 Heat Treat Date
       null, //$25 Lube Apply Date
-      null //$26 Previous Process Tag ID
+      null, //$26 Previous Process Tag ID
+      ProductItem.prd_taglotid //$27 Tag Lot ID
     ])
 
   } catch (error) {
@@ -195,13 +195,13 @@ async function insert863Detail(pool, index, InterchangeControl, ShipmentHeaderTe
 //MARK: Measure
 //863 Measure Insert
 
-async function insert863Measure(pool, key, line, heat, mcoil, mcoil2, mea1, mea2, mea3, mea3f, mea4, mea9, mchr, spsc, sdir, posc, meth, agq, dscd, locn, flag) 
+async function insert863Measure(pool, key, line, heat, mcoil, mcoil2, mea1, mea2, mea3, mea3f, mea4, mea9, mchr, spsc, sdir, posc, meth, agq, dscd, locn, flag, taglot) 
 {
 try {      
   //console.log("Inserting Measure: ", key, line, heat, mcoil, mcoil2, mea1, mea2, mea3, mea3f, mea4, mea9, mchr, spsc, sdir, posc, meth, agq, dscd, locn, flag);
   await pool.query( `INSERT INTO public."863_SNF_Measure"(
-    msr_type,msr_key,msr_line,msr_heat,msr_mcoil,msr_mea1,msr_mea2,msr_mea3f,msr_mea3,msr_mea4,msr_mea9,msr_tdat,msr_pdat,msr_mchr,msr_spsc,msr_sdir,msr_posc,msr_meth,msr_agq,msr_dscd,msr_locn,msr_odat,msr_otim,msr_opgm,msr_flow_flag)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)`,
+    msr_type,msr_key,msr_line,msr_heat,msr_mcoil,msr_mea1,msr_mea2,msr_mea3f,msr_mea3,msr_mea4,msr_mea9,msr_tdat,msr_pdat,msr_mchr,msr_spsc,msr_sdir,msr_posc,msr_meth,msr_agq,msr_dscd,msr_locn,msr_odat,msr_otim,msr_opgm,msr_flow_flag,msr_tag_lot)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26)`,
   [
     'O', //$1
     key, //$2
@@ -227,7 +227,8 @@ try {
     parseInt(new Date().toISOString().replace(/\D/g, '').slice(0, 8)),    //$22
     parseInt(new Date().toISOString().replace(/\D/g, '').slice(8, 14)),   //$23
     'O863SNF', //$24
-    flag //$25
+    flag, //$25
+    taglot //$26
   ]);
     }
  
@@ -242,8 +243,8 @@ try {
   //console.log("Inserting Note: ", Note);
   try {   
     await pool.query( `INSERT INTO public."863_SNF_DetailNotes"(
-      dtln_type, dtln_key, dtln_line, dtln_seq, dtln_text, dtln_odat, dtln_otim, dtln_opgm, dtln_flow_flag)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
+      dtln_type, dtln_key, dtln_line, dtln_seq, dtln_text, dtln_odat, dtln_otim, dtln_opgm, dtln_flow_flag, dtln_tag_lot)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`,
     [
       "O", //$1
       InterchangeControl.ictl_edixcontrolnumber, //$2
@@ -253,7 +254,8 @@ try {
       parseInt(new Date().toISOString().replace(/\D/g, '').slice(0, 8)), //$6
       parseInt(new Date().toISOString().replace(/\D/g, '').slice(8, 14)), //$7
       "O863SNF", //$8
-      flag //$9
+      flag, //$9
+      null //$10 Tag Lot ID
     ]);
     }
     catch (error) {

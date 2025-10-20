1786da7e419b3634a39faa1250c914bc7c18080b
STTXjdewitt <jdewitt@sttxna.com>
2025-10-20 13:32:06 -0400

    Chg856outbound (#151)
    * Fixed the  routing search
    
    * Added mill order number and part number
    
    * FixedM 67
    
    * Added Fixes
    
    * Update O856_insert_SNF.js
    
    * Update O856_insert_SNF.js
    
    * Update O856_SNF_crt.js
    
    * Update I856_json_crt.js
    
    * Save load number
    
    * Update EDI_Tables.js
    
    * Update EDI_Tables.js
    
    * Update O856_SNF_crt.js
    
    * Update EDI_Tables.js
    
    * Update O856_SNF_crt.js
    
    * Update O856_SNF_crt.js
    
    * Update O856_SNF_crt.js
    
    * Update O856_insert_SNF.js
    
    * Update O856_SNF_crt.js
    
    * Update O856_insert_SNF.js
    
    * Fixed Stock Transfers and Mill Coil
    
    * Fixed Part number
    
    * OPRF engine and address priority

--- backend/transactions/856/O856_insert_SNF.js
+++ backend/transactions/856/O856_insert_SNF.js
@@ -319,16 +319,16 @@ async function insert856Detail(pool, InterchangeControl, Item, ProductItem, Ship
       ProductItem.prd_vendortagid, //11
       orginalDetail ? orginalDetail.rows[0].dtl_mo : null, //12
       orginalDetail ? orginalDetail.rows[0].dtl_mol : null, //13
-      ProductItem.prd_externalordernumber, //14
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_cpo || orginalDetail.rows[0].dtl_po || orginalDetail.rows[0].dtl_ucpo || ProductItem.prd_externalordernumber : ProductItem.prd_externalordernumber, //14
       ProductItem.prd_externalorderrelease, //15
       null, //16
-      ProductItem.prd_externalorderdate ? ProductItem.prd_externalorderdate : orginalDetail ? orginalDetail.rows[0].dtl_cpod : null, //17
-      ProductItem.prd_externalorderitem, //18
-      orginalDetail ? (orginalDetail.rows[0].dtl_ucpo || null) : null, //19 
-      ProductItem.prd_externalordernumber, //20
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_cpod || orginalDetail.rows[0].dtl_pod || ProductItem.prd_externalorderdate : ProductItem.prd_externalorderdate ? ProductItem.prd_externalorderdate : orginalDetail ? orginalDetail.rows[0].dtl_cpod : null, //17
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_cpol || orginalDetail.rows[0].dtl_pol : ProductItem.prd_externalorderitem, //18
+      orginalDetail ? (orginalDetail.rows[0].dtl_ucpo || null) : null, //19
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_po || orginalDetail.rows[0].dtl_cpo || ProductItem.prd_externalordernumber : ProductItem.prd_externalordernumber, //20
       null, //21
-      ProductItem.prd_externalorderdate ? ProductItem.prd_externalorderdate : orginalDetail ? orginalDetail.rows[0].dtl_cpod : null, //22
-      ProductItem.prd_externalorderitem ? ProductItem.prd_externalorderitem : orginalDetail ? orginalDetail.rows[0].dtl_cpol : null, //23
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_pod || orginalDetail.rows[0].dtl_cpod || ProductItem.prd_externalorderdate : ProductItem.prd_externalorderdate? ProductItem.prd_externalorderdate : orginalDetail ? orginalDetail.rows[0].dtl_cpod : null, //22
+      ShipmentHeader.ish_shipmentqualifier === 'P' && orginalDetail ? orginalDetail.rows[0].dtl_pol || orginalDetail.rows[0].dtl_cpol || ProductItem.prd_externalorderitem : ProductItem.prd_externalorderitem ? ProductItem.prd_externalorderitem : orginalDetail ? orginalDetail.rows[0].dtl_cpol : null, //23
       ProductItem.prd_rls, //24 Need to be defined
       ProductItem.prd_partnumber, //25
       ProductItem.prd_weight_type === 'A' && ProductItem.prd_weight_um === 'LB' ? ProductItem.prd_weight : null, //26

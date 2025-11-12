27fa360f3cde36ab58c1c05039bec468c914ea88
STTXskadam <sumitk@globalnestsolutions.com>
2025-11-13 02:11:10 +0530

    861 integration outbound (#214)
    * Moving to QA
    
    Moving to QA
    
    * O861 issue fix
    
    O861 issue fix
    
    * Moving N1*OU issue fix into QA
    
    Moving N1*OU issue fix into QA
    
    * Moving O861 BOL loading issue in QA
    
    Moving O861 BOL loading issue in QA
    
    * Moving O861 changes into QA
    
    * Moving to QA
    
    * Moving to QA
    
    Moving to QA
    
    * Moving to QA

--- backend/transactions/861/O861_insert_SNF.js
+++ backend/transactions/861/O861_insert_SNF.js
@@ -253,13 +253,13 @@ async function insert861Detail(pool, InterchangeControl, Item, ProductItem, Rece
       ProductItem.prd_wgt_typ === 'T' && ProductItem.prd_x12actualweightum === 'KG' ? parseInt(ProductItem.prd_actualweight, 10) : null, //37 dtl_twgtkg
       ProductItem.prd_x12gaugeum.includes('ED', 'E8', 'EM', 'E7', 'IN') ? ProductItem.prd_gaugesize : ProductItem.prd_x12widthum.includes('MM', 'MB', 'M2', 'MZ', 'MY')? ProductItem.prd_gaugesize / 25.4 : orginalDetail ? orginalDetail.rows[0].dtl_gaugin : null, //38 dtl_gaugin
       ProductItem.prd_x12gaugeum.includes('MM', 'MB', 'M2', 'MZ', 'MY') ? ProductItem.prd_gaugesize : ProductItem.prd_x12widthum.includes('ED', 'E8', 'EM', 'E7', 'IN')? ProductItem.prd_gaugesize * 25.4 : orginalDetail ? orginalDetail.rows[0].dtl_gaugmm : null, //39 dtl_gaugmm  
-      ProductItem.prd_x12gaugeum === 'ED' ? 'NOM' : ProductItem.prd_x12gaugeum === 'EM' ? 'MIN' : null, //40 dtl_gaugt
+      ProductItem.prd_x12gaugeum.includes('ED', 'MB') ? 'NOM' : ProductItem.prd_x12gaugeum.includes('EM', 'MZ') ? 'MIN' : null, //40 dtl_gaugt
       ProductItem.prd_x12widthum === 'IN' ? ProductItem.prd_width : null, //41 dtl_widin
       ProductItem.prd_x12widthum === 'MM' ? ProductItem.prd_width : null, //42 dtl_widmm
       ProductItem.prd_x12lengthum === 'IN' && ProductItem.prd_length > 0 ? ProductItem.prd_length : null, //43 dtl_ulenin
       ProductItem.prd_x12lengthum === 'MM' && ProductItem.prd_length > 0 ? ProductItem.prd_length : null, //44 dtl_ulenmm
       ProductItem.prd_x12coillengthum.includes('FT', 'LF') ? ProductItem.prd_coillength : ProductItem.prd_x12coillengthum.includes('MT', 'MR')? ProductItem.prd_coillength * 3.28084 : orginalDetail ? orginalDetail.rows[0].dtl_lnft : null, //45 dtl_lnft
-      ProductItem.prd_x12coillengthum.includes('MT', 'MR') ? ProductItem.prd_coillength : ProductItem.prd_x12coillengthum.includes('FT', 'LF')? ProductItem.prd_coillength / 3.28084 : orginalDetail ? orginalDetail.rows[0].dtl_lnft : null, //46 dtl_lnmt
+      ProductItem.prd_x12coillengthum.includes('MT', 'MR') ? ProductItem.prd_coillength : ProductItem.prd_x12coillengthum.includes('FT', 'LF')? ProductItem.prd_coillength / 3.28084 : orginalDetail ? orginalDetail.rows[0].dtl_lnmt : null, //46 dtl_lnmt 
       ProductItem.prd_x12innerdiameterum === 'IN' && ProductItem.prd_innerdiameter > 0 ? ProductItem.prd_innerdiameter : null, //47 dtl_idin
       ProductItem.prd_x12innerdiameterum === 'MM' && ProductItem.prd_innerdiameter > 0 ? ProductItem.prd_innerdiameter : null, //48 dtl_idmm
       ProductItem.prd_x12outerdiameterum === 'IN' && ProductItem.prd_outerdiameter > 0 ? ProductItem.prd_outerdiameter : null, //49 dtl_odin

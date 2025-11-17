ceccc87936fd1a104553f830596182cadf149814
GNSSumit <sumitk@globalnestsolutions.com>
2025-11-17 19:03:22 +0530

    Moving issue fix to QA
    Moving issue fix to QA

--- backend/transactions/863/I863_json_crt.js
+++ backend/transactions/863/I863_json_crt.js
@@ -126,7 +126,7 @@ const formatStructuredJSON = (interchangeControlData, transactionSetData, shipme
 
       const filteredPhysicalTests = PhysicalTests.filter(pt => pt.linenumber === prod.itemnumber).map(pt => {
         pt.linenumber = Number(pt.linenumber);
-        pt.value = Number(pt.value); // Ensure value is set in PhysicalTests
+        pt.value = ['RK', '181', '183', 'RJ', 'KA'].includes(pt.x12physicaltest) ? Math.round(Number(pt.value)) : Number(pt.value); // Ensure value is set in PhysicalTests
         const { linenumber: physicalTestItemNumber, ...physicalwitoutitemnumber } = pt;
         return physicalwitoutitemnumber;
       });

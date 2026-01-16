78c4a1a478298de04896e707faef72b07b5b73b5
STTXjdewitt <jdewitt@sttxna.com>
2026-01-16 16:28:03 -0500

    fixed file path

--- backend/writeJSON.js
+++ backend/writeJSON.js
@@ -3,23 +3,13 @@ const path = require('path');
 const fs = require('fs').promises;
 
 async function writeStructuredJSON(structured, originalName, outputDir, ext = '.txt') {
-  outputDir = process.env.REACT_APP_CLEO_PATH;
+  outputDir = '/mnt/edifiles/JSONS';
   const baseName = path.parse(originalName).name;
   const filePath = path.join(outputDir, `${baseName}${ext}`);
-  const tempPath = path.join('/mnt/edifiles/SNFS', 'JSONS', `${baseName}${ext}`);
 
-  await fs.writeFile(tempPath, JSON.stringify(structured, null, 2));
+  await fs.writeFile(filePath, JSON.stringify(structured, null, 2));
 
-  //const psCmd = `powershell Copy-Item -Path "${tempPath}" -Destination "${filePath}"`;
-  //exec(psCmd, (err, stdout, stderr) => {
-  //  if (err) {
-  //    console.error('PowerShell copy error:', err);
-  //  } else {
-      console.log('Structured JSON written to:', filePath);
-      console.log('Temporary file created at:', tempPath);
-  //  }
-  //  fs.unlink(tempPath);
-  //});
+  console.log('✅ Structured JSON written to:', filePath);
 }
 
 module.exports = { writeStructuredJSON };

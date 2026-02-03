730a7db79a398a6409d739872249a2b00ee40eae
STTXjdewitt <jdewitt@sttxna.com>
2026-02-03 15:11:02 -0500

    fixed java

--- backend/as400/as400connection.js
+++ backend/as400/as400connection.js
@@ -1,20 +1,24 @@
 const { spawn } = require('child_process');
+const path = require('path');
+const os = require('os');
 
 function queryAS400Java(sql) {
     return new Promise((resolve, reject) => {
-        const java = spawn('java', [
-            '-cp',
-            '.\\as400;.\\as400\\java\\jt400.jar;.\\as400\\java\\json.jar',
-            'AS400Query',
-            process.env.REACT_APP_AS400_URL,
-            process.env.REACT_APP_AS400_USER,
-            process.env.REACT_APP_AS400_PASSWORD,
-            sql
-        ]);
+        // Build cross-platform classpath
+        const sep = os.platform() === 'win32' ? ';' : ':';
+        const cp = [
+            path.join(__dirname, 'AS400'),
+            path.join(__dirname, 'java', 'jt400.jar'),
+            path.join(__dirname, 'java', 'json.jar')
+        ].join(sep);
+
+        const args = ['-cp', cp, 'AS400Query', process.env.REACT_APP_AS400_URL, process.env.REACT_APP_AS400_USER, process.env.REACT_APP_AS400_PASSWORD, sql];
+
+        const java = spawn('java', args, { cwd: path.join(__dirname) });
         let output = '';
         let error = '';
-        java.stdout.on('data', data => output += data);
-        java.stderr.on('data', data => error += data);
+        java.stdout.on('data', data => output += data.toString());
+        java.stderr.on('data', data => error += data.toString());
         java.on('close', code => {
             if (code === 0) {
                 try {
@@ -23,7 +27,7 @@ function queryAS400Java(sql) {
                     reject('Failed to parse output: ' + output);
                 }
             } else {
-                reject(error);
+                reject(error || `java exited with code ${code}`);
             }
         });
     });

0ca13d76188524dfecb2fa1929af8ca419a4a664
STTXjdewitt <jdewitt@sttxna.com>
2026-02-03 14:44:54 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -162,7 +162,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.get('/', async (req, res) => {
   const sql = `SELECT * FROM POSHIP`
-  const result = await queryAS400Java(sql)
+  const result = sql //await queryAS400Java(sql)
   res.send('SNF Decoder Backend is running', result);
 });
 // API endpoint to upload inbound SNF files

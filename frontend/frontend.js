74682624c70ddcfcda9a37dd866f2f9fd50db4cf
STTXjdewitt <jdewitt@sttxna.com>
2026-03-31 10:53:21 -0400

    Update frontend.js

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
 // Import other routes (add your route files here)
 // const exampleRoutes = require('./routes/example');
 // app.use('/api/example', exampleRoutes);

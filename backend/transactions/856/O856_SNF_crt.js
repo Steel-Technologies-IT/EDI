1580422cd7bed959e8ae1d5d751327590e03b73e
STTXrsoman <rhushikeshs@globalnestsolutions.com>
2026-06-16 19:31:42 +0530

    Fix I830 fetching for the correct TPID

--- backend/transactions/856/O856_SNF_crt.js
+++ backend/transactions/856/O856_SNF_crt.js
@@ -45,7 +45,8 @@ if (uniqueHeaders.length > 0) {
    let _850 = _850_results.rows;
    let _860_results = await get860forreference(pool, Detail[0].dtl_cpart, Detail[0].dtl_po, Detail[0].dtl_pol, Detail[0].dtl_rls, Header.hdr_isnd_id, '', null);
    let _860 = _860_results.rows;
-   let _830_results = await get830forreference(pool, Detail[0].dtl_cpart, Header.crt_dte, Header.hdr_isnd_id);
+   let isa_rcv_id = await evaluatePriority(priority_1, priority_2, Header.hdr_ircv_id, 'ISA Receiver ID', 'CT')
+   let _830_results = await get830forreference(pool, Detail[0].dtl_cpart, Header.crt_dte, isa_rcv_id);
    let _830 = _830_results.rows;
   
    console.log("Checking for multiple SNFs for pkey:", CustomerID);
